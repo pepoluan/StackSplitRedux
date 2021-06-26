64fca409594459c4c3a49d85e80008afde6af981
Pandu POLUAN <pepoluan@gmail.com>
2021-06-26 16:35:23 +0700

    Add XMLDocs

--- StackSplit-Redux/ModConfig.cs
+++ StackSplit-Redux/ModConfig.cs
@@ -21,8 +21,14 @@ namespace StackSplitRedux
     /// <remarks>Gathered here so we can tune the mod from one place, instead of hunting down config knobs everywhere</remarks>
     internal static class StaticConfig
         {
+        /// <summary>Valid modifier keys to hold while RightClick-ing</summary>
         internal readonly static SButton[] ModifierKeys = new[] { SButton.LeftShift, SButton.RightShift };
+
+        /// <summary>Delay between new menu appearing & our handler beginning</summary>
+        /// <remarks>To allow time for other mods to manipulate inventories</remarks>
         internal readonly static int SplitMenuOpenDelayTicks = 2;
+
+        /// <summary>Mods that we are conflicting with. If found, abort this mod</summary>
         internal readonly static string[] ConflictingMods = new [] { "tstaples.StackSplitX" };
 
         /// <summary>Text color when the text is highlighted. This should contrast with HighlightColor.</summary>
